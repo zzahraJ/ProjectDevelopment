8ec33e7bb14b22a71dbe55a9c35854b45422ad73
brittadam <britt.adam@hotmail.com>
2023-05-11 17:51:18 +0200

    Load the model

--- sketch.js
+++ sketch.js
@@ -1,16 +1,19 @@
 // Video
 let video;
 
+// Load the model
+function preload() {
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/jVcS5ZSNQ/");
+}
+
 function setup() {
     createCanvas(640, 520);
     // Create the video
     video = createCapture(VIDEO);
     video.hide();
-    // classifyVideo();
 }
 
 function draw() {
     // Draw the video
     image(video, 0, 0);
 }
-
